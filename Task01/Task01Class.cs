19ed8841aebfac5c322099d8ff7f79467ec0218b
Llani <llani.rainey@gmail.com>
2026-03-12 11:27:10 +0000

    Save update files

---
+++ Task01/Task01Class.cs
@@ -0,0 +1,20 @@
+﻿namespace Task01
+{
+    public class Task01Class
+    {
+        public static void Run()
+        {
+            float floatValue = 3.80f;
+            string stringValue = "h";
+            double doubleValue = 2.50;
+
+            floatValue = 3.80;
+            stringValue = 'h';
+            doubleValue = "apple";
+
+
+        }
+
+
+    }
+}
